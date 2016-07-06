f2e39bdbca37b265f4fbda9dec5649c8ab9335d1
MasonSu <xiaxzheng@gmail.com>
2016-07-06 16:06:53 +0800

    finish Part V

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
