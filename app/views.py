69c03913d1dc5bfd3e039bc1bce035e660230815
MasonSu <xiaxzheng@gmail.com>
2016-07-07 16:32:30 +0800

    Part VIII needed to be done

--- app/views.py
+++ app/views.py
@@ -1,26 +1,26 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User
+from forms import LoginForm, EditForm, PostForm
+from models import User, Post
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-	user = g.user
-	posts = [
-		{
-			'author': {'nickname': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'nickname': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}#dict
-	]#list
-	return render_template('index.html', title='', user=user, posts=posts)
+	form = PostForm()
+	if form.validate_on_submit():
+	    post = Post(body=form.post.data, timestamp=datetime.utcnow(), author=g.user)
+	    db.session.add(post)
+	    db.session.commit()
+	    flash('Your post is now live!')
+	    return redirect(url_for('index'))
+	posts = g.user.followed_posts().all()
+	return render_template('index.html',
+	                       title='Home',
+	                       form=form,
+	                       posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
@@ -60,6 +60,9 @@ def after_login(resp):
 		user = User(nickname=nickname, email=resp.email)
 		db.session.add(user)
 		db.session.commit()
+		# make the user follow him/herself
+        db.session.add(user.follow(user))
+        db.session.commit()
 	remember_me = False
 	if 'remember_me' in session:
 		remember_me = session['remember_me']
@@ -103,6 +106,44 @@ def edit():
         form.about_me.data = g.user.about_me
     return render_template('edit.html', form=form)
 
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t follow yourself!')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following ' + nickname + '!')
+    return redirect(url_for('user', nickname=nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname=nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('user', nickname=nickname))
+    
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
